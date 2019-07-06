94307141299c2f7a0033e6cff9122c8dcb9b8f62
Bruno Martins <bgcngm@gmail.com>
2019-07-06 12:54:09 +0100

    sdm845-common: sepolicy: Fix typo in pocketmode_app rules
    Change-Id: I3806e0381eab678ac14f8869819e938b62cbe59d

--- pocketmode/Android.mk
+++ pocketmode/Android.mk
@@ -11,16 +11,6 @@ LOCAL_CERTIFICATE := platform
 LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_PRIVILEGED_MODULE := true
 
-<<<<<<< HEAD
-LOCAL_STATIC_ANDROID_LIBRARIES := \
-    android-support-v4 \
-    android-support-v7-appcompat \
-    android-support-v7-preference \
-    android-support-v7-recyclerview \
-    android-support-v14-preference
-
-=======
->>>>>>> 77847d9... sdm845-common: pocketmode: Kill off Lineage SDK dependency
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 include $(BUILD_PACKAGE)
